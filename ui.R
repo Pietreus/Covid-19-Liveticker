2946e55356d9f45c9a05b2d70d88be3e39334c33
Peter Blohm <peter.blohm@outlook.at>
2020-08-03 18:48:35 +0200

    Add tipify Tooltips as documentation

--- ui.R
+++ ui.R
@@ -9,22 +9,25 @@
 library(shiny)
 library(leaflet)
 library(plotly)
-
+library(shinyBS)
 # Define UI for application that draws a histogram
 shinyUI({
   fluidPage(title ="Covid-19 Liveticker",
-    tabsetPanel(id = "dataType",
-                tabPanel("Total Infections",value = "Cases"),
-                tabPanel("Active Cases",value = "Active"),
-                tabPanel("Total Deaths",value = "Deaths")),
-    sidebarLayout(
-        sidebarPanel(
-          uiOutput("countryVector"),
-          htmlOutput("summary"),
-          radioButtons("plotType","Plottype",c("Total","Daily"), selected = "Total"),
-          conditionalPanel(
-            condition = "input.plotType != 'Daily'",radioButtons("scaleType","Scale",list("linear","log"), selected="log"))),
-        mainPanel(
-            leafletOutput("worldMap"),
-            plotlyOutput("detailPlot"))))
-  })
+            tipify(tabsetPanel(id = "dataType",
+                               tabPanel("Total Infections",value = "Cases"),
+                               tabPanel("Active Cases",value = "Active"),
+                               tabPanel("Total Deaths",value = "Deaths")),
+                   title = "change heatmap coloring"),
+            sidebarLayout(
+              sidebarPanel(
+                tipify(uiOutput("countryVector"),title = "select country via map or dropdown menu"),
+                htmlOutput("summary"),
+                tipify(radioButtons("plotType","Displayed Plot",c("Total","Daily"), selected = "Total"),
+                       title = "Change the type of covid-19 cases displayed in the Plot"),
+                tipify(conditionalPanel(
+                  condition = "input.plotType != 'Daily'",radioButtons("scaleType","y-axis scaling",list("linear","log"), selected="log")),title = "Change the y-axis scaling")),
+              mainPanel(
+                tipify(leafletOutput("worldMap"),
+                       title = "select Country for Information"),
+                plotlyOutput("detailPlot"))))
+})
